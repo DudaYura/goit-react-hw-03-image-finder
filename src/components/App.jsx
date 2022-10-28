dee4d2811e8dcfe1256be3fb9e1f60f06431f7d4
DudaYura <YurkaD@ukr.net>
2022-10-28 18:54:09 +0300

    выполнил задание

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,85 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { fetchImages } from 'services/api.jsx';
+import { Searchbar } from 'components/Searchbar/Searchbar.jsx';
+import { ImageGallery } from 'components/ImageGallery/ImageGallery.jsx';
+import { Loader } from 'components/Loader/Loader.jsx';
+import { Button } from 'components/Button/Button.jsx';
+import { Container, Notify } from './App.styled';
+
+export class App extends Component {
+  state = {
+    query: '',
+    page: 1,
+    images: null,
+    isLoading: false,
+    total: 0,
+  };
+
+  componentDidUpdate(_, prevState) {
+    const { page, query } = this.state;
+    if (prevState.page !== page || prevState.query !== query) {
+      this.dataImages(query, page);
+    }
+  }
+
+  handleInput = e => {
+    this.setState({
+      page: 1,
+      query: e.searchQuery,
+      images: null,
+    });
+  };
+
+  loadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+      isLoading: true,
+    }));
+  };
+
+  dataImages = async (query, page) => {
+    this.setState({ isLoading: true });
+    const data = await fetchImages(query, page);
+
+    if (page === 1) {
+      this.setState(() => ({
+        total: data.total,
+        images: [...data.hits],
+        isLoading: false,
+      }));
+    } else {
+      this.setState(state => ({
+        images: [...state.images, ...data.hits],
+        isLoading: false,
+      }));
+    }
+  };
+
+  render() {
+    const { images, isLoading, total } = this.state;
+    return (
+      <Container>
+        <Searchbar onSubmit={this.handleInput} />
+        {isLoading && <Loader>Loading</Loader>}
+
+        {images && (
+          <>
+            {images.length === 0 && <Notify>Pictures not found</Notify>}
+
+            <ImageGallery items={images} />
+
+            {isLoading && <Loader>Loading</Loader>}
+            {images.length > 0 && images.length < total && (
+              <Button onLoadMore={this.loadMore} />
+            )}
+            {isLoading && <Loader>Loading</Loader>}
+
+            {images.length === total && !!images.length && (
+              <Notify>No more pictures</Notify>
+            )}
+          </>
+        )}
+      </Container>
+    );
+  }
+}
